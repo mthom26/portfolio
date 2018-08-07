6adb9569e5a1446ebb1d4de1f96a45289bc1c5d2
mthom26 <mthom2626@gmail.com>
2018-08-07 16:24:56 +0100

    Add hover effect to ProjectCard

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,13 +3,16 @@ import React from 'react';
 const ProjectCard = () => {
   return (
     <div className="projectCard">
-      <div>
-        <img src="https://picsum.photos/200/?random" />
+      <div className="projectCardContent">
+        <div>
+          <img src="https://picsum.photos/200/?random" />
+        </div>
+        <div>
+          <h3>Project Name</h3>
+          <p>Short project description...</p>
+        </div>
       </div>
-      <div>
-        <h3>Project Name</h3>
-        <p>Short project description...</p>
-      </div>
+      <div className="projectCardBar" />
     </div>
   );
 };
